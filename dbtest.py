6a298fe8fb3e29215e4251dfe09ac95ffb19ca3e
lgb <3417505@qq.com>
2019-10-29 11:33:12 +0800

    Initial commit

---
+++ dbtest.py
@@ -0,0 +1,13 @@
+from dbsqlachemy import db,User1
+
+# db.create_all()
+# admin = User1('admin', 'admin@example.com')
+# guest = User1('guest', 'guest@example.com')
+# db.session.add(admin)
+# db.session.add(guest)
+# db.session.commit()
+
+users = User1.query.all()
+print(users)
+admin = User1.query.filter_by(username='admin').first()
+print(admin)
